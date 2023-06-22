8941ff9606733fd5f4eba2504186d799772d9f3b
Rabih Aslam K <azaslam121@gmail.com>
2023-06-22 19:24:13 +0530

    live url added

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -1,5 +1,9 @@
 import styles from "./index.module.css";
 
 export default function Footer() {
-  return <>Footer</>;
+  return (
+    <>
+      <div className="flex items-center justify-center h-16">Made by 61-5a</div>
+    </>
+  );
 }
--- src/components/sections/secThreeHome/index.js
+++ src/components/sections/secThreeHome/index.js
@@ -34,7 +34,7 @@ export default function SecThreeHome() {
     axios
       .get(`https://fakestoreapi.com/products/category/${item}`)
       .then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
         setData(response.data);
         setListview(false);
       })
@@ -65,11 +65,11 @@ export default function SecThreeHome() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
                   ></path>
                 </svg>
